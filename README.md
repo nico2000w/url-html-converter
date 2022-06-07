eb7c0d1b5f94d8ca75be3d5f3526a5864ff700cc
Nicolás Martín Iriburo Victorino <nico1039tac@gmail.com>
2022-06-07 23:10:27 +0200

    Repository updated succesfully

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-This will be the readme file
+#BASH SCRIPT FOR CONVERTING URL SHORTCUTS FILES THAT BROWSERS CANNOT OPEN 
+
+#This script converts to HTML files that can be opened by anywhere browser with only two clicks 
+#without necessity to create a new .desktop file.Its not needed some installation, only take the 
+#script into the folder that exists the file. And follow the next steps to execute: 
+
+chmod +x ./url-html-converter.sh
+
+./url-html-converter.sh
+
---
+++ url-html-converter.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo -e "############################################" 
+echo -e "            URL TO HTML CONVERTER           "
+echo -e "############################################"
+echo -e ""
+echo -e ""
+
+find . -name "*.url" > /tmp/files_url.txt
+n=1
+while read line;do
+	echo -e "["${n}"]" ${line:2} 
+	n=$((n+1))
+done < /tmp/files_url.txt
+n=1
+
+echo -e ""
+echo -e ""
+echo -e "Choose a file for converter to html: "
+read file
+
+search_file=`sed -n ${file}p /tmp/files_url.txt`
+while read line; do
+	if [[ $line == *"URL="* ]]; then
+		url=${line:4}
+		echo $url
+	fi
+done < $search_file
+
+final_file=${search_file:2:-3}
+final_file=`echo -e ${final_file}"html"`
+touch $final_file
+echo -e "<html>" >> $final_file
+echo -e "	<head>" >> $final_file
+echo -e '		<meta http-equiv="refresh" content="0; url='${url}'" />' >> $final_file
+echo -e "	</head>" >> $final_file
+echo -e "	<body></body>" >> $final_file
+echo -e "</html>" >> $final_file
+echo -e ""
+echo -e ""
+echo -e "FILE HTML CREATED SUCCESFULLY"
+
+
